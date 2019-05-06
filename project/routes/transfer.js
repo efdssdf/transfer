a59e96044caba7c9c7962e002260e8cb3e2ad864
Administrator <lionel@buzzy.cc>
2019-05-06 14:58:24 +0800

    1

--- project/routes/transfer.js
+++ project/routes/transfer.js
@@ -8,17 +8,48 @@ router.get('/:id', function (req, res, next) {
     mem.get('transfer_' + id).then(function (value) {
         if(value){
             value = JSON.parse(value)
-            var link = value.links[Math.floor(Math.random() * value.links.length)]
-            //console.log('----li----', link)
+            var links = []
+            if(value.type==1){
+                for (var i = 0; i < value.links.length; i++) {
+                    var link = value.links[i];
+                    var weight =10;
+                    if(value.weights>i){
+                        weight = value.weights[i]
+                    }
+                    for (var j = 0; j < weight; j++) {
+                        links.push(link)
+                    }
+                }
+            }else{
+                links = value.links
+            }
+            var link = links[parseInt(Math.random()*links.length)]
             res.redirect(link)
         }else {
             TransferModel.find({id: id}, function (err, data) {
                 if (data && data[0]&& data[0].links) {
-                    var link = data[0].links[Math.floor(Math.random() * data[0].links.length)]
-                    mem.set('transfer_' + req.params.id, JSON.stringify(data[0]), 1*60).then(function () {
+                    var value = data[0];
+                    mem.set('transfer_' + req.params.id, JSON.stringify(value), 1*60).then(function () {
                         //console.log('---------set transfer value---------')
                     })
+                    var links = []
+                    if(value.type==1){
+                        for (var i = 0; i < value.links.length; i++) {
+                            var link = value.links[i];
+                            var weight =10;
+                            if(value.weights>i){
+                                weight = value.weights[i]
+                            }
+                            for (var j = 0; j < weight; j++) {
+                                links.push(link)
+                            }
+                        }
+                    }else{
+                        links = value.links
+                    }
+                    
                     //console.log('----lixin----', link)
+                    var link = links[parseInt(Math.random()*links.length)]
                     res.redirect(link)
                 }else{
                     res.send('没有查询到此链接，请先创建')
