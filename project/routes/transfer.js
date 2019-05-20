e8f13294f1aec0685e62175378397a7ad1a0b3b0
Administrator <lionel@buzzy.cc>
2019-05-20 13:23:07 +0800

    1

--- project/routes/transfer.js
+++ project/routes/transfer.js
@@ -3,8 +3,14 @@ var router = express.Router();
 var TransferModel = require('../model/Transfer.js');
 var mem = require('../util/mem.js')
 
+const asyncRedis = require("async-redis");
+const redis_client = asyncRedis.createClient();
+
 router.get('/:id', function (req, res, next) {
     var id = req.params.id;
+
+    statics(req,res)
+
     mem.get('transfer_' + id).then(function (value) {
         if(value){
             value = JSON.parse(value)
@@ -63,6 +69,60 @@ router.get('/:id', function (req, res, next) {
         console.log(err);
     });
 })
+
+function statics(req,res){
+    var id = req.params.id;
+    var material = req.query.material;
+    if(!material){
+        return
+    }
+    let uid = req.cookies['transfer_novel_b'];
+    if(!uid){
+        uid = randomWord(false,32)
+        res.cookie(
+            'transfer_novel_b',uid,{
+                path:'/',       // 写cookie所在的路径
+                maxAge: 100*12*30*24*60*60*1000,   // cookie有效时长
+                expires:new Date(Date.now()+100*12*30*24*60*60*1000), // cookie失效时间
+                httpOnly:false,  // 是否只用于http请求中获取
+                overwrite:false  // 是否允许重写
+            }
+        );
+    }
+
+
+    redis_client.incr('website_transfer_pv_'+id+'_'+material).then(function(){})
+    redis_client.pfadd('website_transfer_uv_'+id+'_'+material , uid).then(function(){})
+    redis_client.pfadd('website_transfer_ip_'+id+'_'+material , getClientIp(req)).then(function(){})
+    
+    //统计
+    
+
+}
+
+let getClientIp = function (req) {
+    return req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress || '';
+}
+
+function randomWord(randomFlag, min, max){
+    var str = "",
+        range = min,
+        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+ 
+    // 随机产生
+    if(randomFlag){
+        range = Math.round(Math.random() * (max-min)) + min;
+    }
+    for(var i=0; i<range; i++){
+        pos = Math.round(Math.random() * (arr.length-1));
+        str += arr[pos];
+    }
+    return str;
+}
+
 
 function getLink(req,value,link){
     if(value.back_urls&&value.back_urls.length){
